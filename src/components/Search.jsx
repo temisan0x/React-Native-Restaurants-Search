a8e0b3d7a14d4da015195dbb78507951ff5c8943
Heytemisan <temyicy@gmail.com>
2022-06-03 00:12:21 +0100

    feat: created an array of styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,10 @@
 import { View, TextInput, StyleSheet } from 'react-native';
-import {FontAwesome} from "@expo/vector-icons"
+import { FontAwesome } from "@expo/vector-icons";
+import {elevation} from "../styles/styles"
 
 export default function Search() {
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, styles.elevation]}>
             <FontAwesome name='search' size={25}/>
             <TextInput placeholder="Restaurant food" style={ styles.input}/>
         </View>
@@ -16,14 +17,10 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginHorizontal: 15,
         backgroundColor: "white",
-        shadowColor: "black",
-        shadowOffset:{width: 5, height: 5},
-        elevation: 3,
-        shadowOpacity: 0.1,
         padding: 15,
         borderRadius: 15,
     },
-
+    elevation,
     input: {
         fontSize:20,
         marginLeft: 15 ,
