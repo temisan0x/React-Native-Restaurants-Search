337000ff08504c8b27593dc5a4335e7b95ecb1c4
Heytemisan <temyicy@gmail.com>
2022-06-03 01:58:20 +0100

    feat: added additional image

--- App.js
+++ App.js
@@ -4,6 +4,40 @@ import Header from "./src/components/Header";
 import Search from "./src/components/Search.jsx";
 
 export default function App() {
+
+  //FlatList 
+  categoriesData = [
+    {
+      name: "Beans and Plantain",
+      imageUrl: require("./src/assets/images/beans.jpeg"),
+    },
+    {
+      name: "Eba and Egusi",
+      imageUrl: require("./src/assets/images/eba.jpeg"),
+    },
+    {
+      name: "Chapman drink",
+      imageUrl: require("./src/assets/images/chapman.jpeg"),
+    },
+    {
+      name: "Jollof Rice",
+      imageUrl: require("./src/assets/images/jollof.jpeg"),
+    },
+    {
+      name: "Curry Sauce",
+      imageUrl: require("./src/assets/images/sauce.jpeg"),
+    },
+    {
+      name: "Zobo Drink",
+      imageUrl: require("./src/assets/images/zobo.jpeg"),
+    },
+    {
+      name: "Yam and Egg",
+      imageUrl: require("./src/assets/images/yam.jpeg"),
+    }
+  ];
+
+
   return (
     <View styles={styles.container}>
       <Header />
